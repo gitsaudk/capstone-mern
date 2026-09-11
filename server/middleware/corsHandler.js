3aec9313f558cafd5b002f0b1c484d393b723c90
git <saudkokate59@gmail.com>
2026-09-11 20:59:08 +0530

    cors change

--- server/middleware/corsHandler.js
+++ server/middleware/corsHandler.js
@@ -1,7 +1,18 @@
 const cors = require("cors");
 
+const allowedOrigins = (process.env.CLIENT_URL || "")
+    .split(",")
+    .map((origin) => origin.trim().replace(/\/$/, ""))
+    .filter(Boolean);
+
 const corsHandler = cors({
-    origin: process.env.CLIENT_URL,
+    origin: (requestOrigin, callback) => {
+        if (!requestOrigin || allowedOrigins.includes(requestOrigin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Origin is not allowed by CORS"));
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
