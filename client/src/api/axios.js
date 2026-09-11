9c34a2e824a30f36fc61a6804214c3a847f2ef26
git <saudkokate59@gmail.com>
2026-09-11 20:12:29 +0530

    Fix production API base path

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const configuredApiUrl = import.meta.env.VITE_API_URL || "";
+const apiBaseUrl = configuredApiUrl.replace(/\/$/, "").endsWith("/api")
+  ? configuredApiUrl.replace(/\/$/, "")
+  : `${configuredApiUrl.replace(/\/$/, "")}/api`;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: apiBaseUrl,
 });
 
 api.interceptors.request.use((config) => {
